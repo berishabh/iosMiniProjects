9f9c420ca7c005cb81d419c08b094f94813b3bfd
Rishabh Kumar <integersort@gmail.com>
2023-05-02 23:36:29 +0530

    Add files via upload

---
+++ learnUIKit/SecondScreen.swift
@@ -0,0 +1,22 @@
+//
+//  SecondScreen.swift
+//  learnUIKit
+//
+//  Created by Rishabh  Kumar on 23/04/23.
+//
+
+import UIKit
+
+class SecondScreen: UIViewController {
+
+    let items = ["Apple,Guava,Banana,Mangoes,Grapes"]
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let view = UIView()
+        view.backgroundColor = .white
+        
+        
+    }
+}
